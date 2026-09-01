5e3dd6cc786473ec332e80d313e09a172259af56
vixhin <vi.maulen@duocuc.cl>
2026-09-01 11:10:31 -0400

    Inicio proyecto

---
+++ pedidos360-backend/src/main/java/cl/duoc/pedidos360/Pedidos360BackendApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.pedidos360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pedidos360BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pedidos360BackendApplication.class, args);
+	}
+
+}
---
+++ pedidos360-backend/src/test/java/cl/duoc/pedidos360/Pedidos360BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.pedidos360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pedidos360BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
